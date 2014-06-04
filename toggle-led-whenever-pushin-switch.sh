05df4897eec0e387a9b8b654caf495d491ef3c88
Yasuharu Nakano <ynak@jggug.org>
2014-06-05 02:03:01 +0900

    avoiding chattering

--- toggle-led-whenever-pushin-switch.sh
+++ toggle-led-whenever-pushin-switch.sh
@@ -18,19 +18,26 @@ echo $LED_GPIO_NUM > $GPIO_DIR/export
 LED_GPIO_DIR=$GPIO_DIR/gpio$LED_GPIO_NUM
 echo out > $LED_GPIO_DIR/direction
 
+# Main
 led_status=0
+previous_switch_status=0
 while true
 do
 	# Whenever pushing a task switch, toggle to turn on LED and off.
 	if [ $( cat $SWITCH_GPIO_DIR/value ) -eq 1 ]; then
-		if [ $led_status -eq 0 ]; then
-			echo 1 > $LED_GPIO_DIR/value
-			led_status=1
-		else
-			echo 0 > $LED_GPIO_DIR/value
-			led_status=0
+		if [ $previous_switch_status -eq 0 ]; then
+			if [ $led_status -eq 0 ]; then
+				echo 1 > $LED_GPIO_DIR/value
+				led_status=1
+			else
+				echo 0 > $LED_GPIO_DIR/value
+				led_status=0
+			fi
 		fi
+		previous_switch_status=1
+	else
+		previous_switch_status=0
 	fi
-	sleep .1
+	sleep .0625
 done
 
